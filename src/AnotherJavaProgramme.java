956d40358546debe249a0301dcc4c23d4cc126a5
dipali <dipalispatil2207@gmail.com>
2023-02-02 09:38:04 +0530

    Commit another java program

---
+++ src/AnotherJavaProgramme.java
@@ -0,0 +1,10 @@
+
+public class AnotherJavaProgramme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HEllo From Another Program");
+
+	}
+
+}
